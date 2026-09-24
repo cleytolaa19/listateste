bf5d62ae75f164c3ff62b52112e0758f85c71a7d
José Clayton dos Santos Oliveira <20231134010037@ifrn.local>
2026-09-24 10:15:56 -0300

    laboratorio

---
+++ TODOLIST-MVC/app/models/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/app/controller/controller.php';
+$controller = new TarefaController();
+$action = $_GET['action'] ?? 'index';
+
+switch ($action) {
+      case 'criar';
+      $controller->criar();
+      break;
+      case 'excluir':
+        $controller->excluir();
+        break;
+        case 'editar':
+            $controller->editar();
+            break;
+            default;
+            $controller->index();
+            break;
+        
+}
